a54898859b0c8574da29861d75b39edd1530ae62
Emanuel Tesar <e.tesarr@gmail.com>
2021-08-26 21:17:32 +0200

    Fix TS errors

--- packages/node/src/evm/fulfillments/index.ts
+++ packages/node/src/evm/fulfillments/index.ts
@@ -5,7 +5,7 @@ import * as grouping from '../../requests/grouping';
 import * as logger from '../../logger';
 import * as wallet from '../wallet';
 import {
-  ClientRequest,
+  Request,
   EVMProviderState,
   ProviderState,
   RequestStatus,
@@ -70,7 +70,7 @@ export async function submit(state: ProviderState<EVMProviderState>): Promise<Tr
   return receipts;
 }
 
-export function applyFulfillments<T>(requests: ClientRequest<T>[], receipts: TransactionReceipt[]) {
+export function applyFulfillments<T>(requests: Request<T>[], receipts: TransactionReceipt[]) {
   return requests.reduce((acc, request) => {
     const receipt = receipts.find((r) => r.id === request.id);
     // If the request was not submitted or the transaction doesn't have a hash, leave it as is
@@ -84,5 +84,5 @@ export function applyFulfillments<T>(requests: ClientRequest<T>[], receipts: Tra
       status: RequestStatus.Submitted,
     };
     return [...acc, updatedRequest];
-  }, [] as ClientRequest<T>[]);
+  }, [] as Request<T>[]);
 }
